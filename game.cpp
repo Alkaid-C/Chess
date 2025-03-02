9eb5d714f6f80591c9d4034f1184255a816260bb
Alkaid-C <hchen2@fandm.edu>
2025-03-02 05:46:49 -0500

    Add another testgame, and it fails at a pawn move. Will investigate later.

--- game.cpp
+++ game.cpp
@@ -1,12 +1,18 @@
 #include "board.h"
 #include <iostream>
 
+static void testGame1();
+static void testGame2();
 
 int main()
 {
+	testGame1();
+	testGame2();
+}
+static void testGame1()
+{
 	board theboard;
 	theboard.printBoard();
-	cout << "tomove" << endl;
 	theboard.move(2, E, 4, E);
 	theboard.printBoard();
 	theboard.move(7, D, 5, D);
@@ -55,5 +61,60 @@ int main()
 	theboard.printBoard();
 	theboard.move(6, D, 3, G);
 	theboard.printBoard();
-	return 0;
+	return ;
+}
+
+static void testGame2()
+{
+	board theboard;
+	theboard.printBoard();
+	theboard.move(2, D, 4, D);
+	theboard.printBoard();
+	theboard.move(7, D, 5, D);
+	theboard.printBoard();
+	theboard.move(1, G, 3, F);
+	theboard.printBoard();
+	theboard.move(8, C, 5, F);
+	theboard.printBoard();
+	theboard.move(2, E, 3, E);
+	theboard.printBoard();
+	theboard.move(8, B, 6, C);
+	theboard.printBoard();
+	theboard.move(1, F, 3, D);
+	theboard.printBoard();
+	theboard.move(5, F, 3, D);
+	theboard.printBoard();
+	theboard.move(2, C, 3, D);
+	theboard.printBoard();
+	theboard.move(8, D, 6, D);
+	theboard.printBoard();
+	theboard.move(1, C, 2, D);
+	theboard.printBoard();
+	theboard.move(8, E, 8, C);
+	theboard.printBoard();
+	theboard.move(1, C, 2, D);
+	theboard.printBoard();
+	theboard.move(7, A, 6, A);
+	theboard.printBoard();
+	theboard.move(2, A, 3, A);
+	theboard.printBoard();
+	theboard.move(8, E, 8, C);
+	theboard.printBoard();
+	theboard.move(2, B, 4, B);
+	theboard.printBoard();
+	theboard.move(6, D, 6, G);
+	theboard.printBoard();
+	theboard.move(3, F, 4, H);
+	theboard.printBoard();
+	theboard.move(6, G, 3, D);
+	theboard.printBoard();
+	theboard.move(4, H, 3, F);
+	theboard.printBoard();
+	theboard.move(1, D, 2, E);
+	theboard.printBoard();
+	theboard.move(3, D, 6, G);
+	theboard.printBoard();
+	theboard.move(1, E, 1, G);
+	theboard.printBoard();
+	return;
 }
